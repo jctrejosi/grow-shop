575b5d7640f6662e7d405d521461f1776518835a
Juan Trejos <juan.trejos@arandasoft.com>
2024-03-09 09:37:23 -0500

    fix routes

--- Backend/routes/api.php
+++ Backend/routes/api.php
@@ -1,19 +1,20 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
+// ---- Controllers
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\ProductController;
+
+// ---- Clients
+
+Route::get('/clients', [ClientController::class, 'index']);
+Route::post('/clients', [ClientController::class, 'store']);
+Route::get('/clients/{id}', [ClientController::class, 'show']);
+Route::put('/clients/{id}', [ClientController::class, 'update']);
+Route::delete('/clients/{id}', [ClientController::class, 'destroy']);
+
+// ---- Products
+
+Route::get('/products', [ProductController::class, 'index'])->name('products.index');
--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -3,19 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ClientController;
-
-Route::group(['prefix' => 'api'], function () {
-    Route::get('/clients', [ClientController::class, 'index']);
-    Route::post('/clients', [ClientController::class, 'store']);
-    Route::get('/clients/{id}', [ClientController::class, 'show']);
-    Route::put('/clients/{id}', [ClientController::class, 'update']);
-    Route::delete('/clients/{id}', [ClientController::class, 'destroy']);
-});
-
-Route::get('/api/products', [ProductController::class, 'index'])->name('products.index');
-
 Route::get('/test-db-connection', function () {
     try {
         DB::connection()->getPdo();
